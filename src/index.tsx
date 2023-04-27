8aa426759ddd545c9e75299d51736f660dfc90cb
igfigueiredo <igfigueiredo@uolinc.com>
2023-04-27 19:13:59 -0300

    Tentando usar o react helmet para tirar a descrição padrão do react

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import {Helmet} from "react-helmet";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+      <Helmet>
+          <title>Academia</title>
+          <meta name="description" content="Auxiliador para o jogo de tabuleiro Academia" />
+      </Helmet>
       <BrowserRouter>
           <App/>
       </BrowserRouter>
